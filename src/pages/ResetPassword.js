4002959755bf95263f4cabdc9f8986d5ba6678a5
Yash <yashrajoriya321@gmail.com>
2023-03-23 15:39:11 +0530

    update

--- src/pages/ResetPassword.js
+++ src/pages/ResetPassword.js
@@ -1,42 +1,42 @@
-// import React from "react";
-// import { RiLockPasswordLine } from "react-icons/ri";
+import React from "react";
+import { RiLockPasswordLine } from "react-icons/ri";
 
-// const ResetPassword = () => {
-//   return (
-//     <section id="section">
-//       <div className="form-box">
-//         <div className="form-value">
-//           <form action="">
-//             <h3 className="text-center title">Reset Password</h3>
-//             <p className="text-center">Please enter your new password</p>
-//             <div className="inputbox">
-//               <RiLockPasswordLine className="ion-icon" />
-//               <input
-//                 type="password"
-//                 name="password"
-//                 label="Password"
-//                 id="pass"
-//                 required
-//               />
-//               <label for="">New Password</label>
-//             </div>
-//             <div className="inputbox">
-//               <RiLockPasswordLine className="ion-icon" />
-//               <input
-//                 type="password"
-//                 name="password"
-//                 label="Password"
-//                 id="pass"
-//                 required
-//               />
-//               <label for="">Confirm Password</label>
-//             </div>
-//             <button>Submit</button>
-//           </form>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
+const ResetPassword = () => {
+  return (
+    <section id="section">
+      <div className="form-box">
+        <div className="form-value">
+          <form action="">
+            <h3 className="text-center title">Reset Password</h3>
+            <p className="text-center">Please enter your new password</p>
+            <div className="inputbox">
+              <RiLockPasswordLine className="ion-icon" />
+              <input
+                type="password"
+                name="password"
+                label="Password"
+                id="pass"
+                required
+              />
+              <label for="">New Password</label>
+            </div>
+            <div className="inputbox">
+              <RiLockPasswordLine className="ion-icon" />
+              <input
+                type="password"
+                name="password"
+                label="Password"
+                id="pass"
+                required
+              />
+              <label for="">Confirm Password</label>
+            </div>
+            <button>Submit</button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-// export default ResetPassword;
+export default ResetPassword;
