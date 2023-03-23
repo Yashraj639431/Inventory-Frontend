11c2ab543bc22ea8f31686064c48782914780c5f
Yash <yashrajoriya321@gmail.com>
2023-03-23 15:24:26 +0530

    update

--- src/App.js
+++ src/App.js
@@ -5,20 +5,6 @@ import ResetPassword from "./pages/ResetPassword";
 import ForgotPassword from "./pages/ForgotPassword";
 import MainLayout from "./components/MainLayout";
 import Dashboard from "./pages/Dashboard";
-// import ElementColor from "./pages/ElementColor";
-import Category from "./pages/Category";
-import Items from "./pages/Item";
-// import Element from "./pages/Element";
-import Warehouse from "./pages/Warehouse";
-import AddProduct from "./pages/AddProduct";
-import ManageProduct from "./pages/ManageProduct";
-import AddOrder from "./pages/AddOrder";
-import ManageOrder from "./pages/ManageOrder";
-import AddMember from "./pages/AddMember";
-import ManageMember from "./pages/ManageMember";
-import AddPermission from "./pages/AddPermission";
-import ManagePermission from "./pages/ManagePermission";
-import Company from "./pages/Company";
 
 function App() {
   return (
@@ -29,23 +15,6 @@ function App() {
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="admin" element={<MainLayout />}>
           <Route index element={<Dashboard />} />
-          <Route path="item" element={<Items />} />
-          <Route path="category" element={<Category />} />
-          <Route path="warehouse" element={<Warehouse />} />
-          {/* <Route path="element" element={<Element />} /> */}
-          {/* <Route path="element/:id" element={<Element />} /> */}
-          {/* <Route path="element-color/" element={<ElementColor />} /> */}
-          {/* <Route path="element-color/:id" element={<ElementColor />} /> */}
-          {/* <Route path="element/size/:id" element={<Element />} /> */}
-          <Route path="add-product" element={<AddProduct />} />
-          <Route path="manage-product" element={<ManageProduct />} />
-          <Route path="add-order" element={<AddOrder />} />
-          <Route path="manage-order" element={<ManageOrder />} />
-          <Route path="add-member" element={<AddMember />} />
-          <Route path="manage-member" element={<ManageMember />} />
-          <Route path="add-permission" element={<AddPermission />} />
-          <Route path="manage-permission" element={<ManagePermission />} />
-          <Route path="company" element={<Company />} />
         </Route>
       </Routes>
     </Router>
